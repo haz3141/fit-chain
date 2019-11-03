8a9dcf9754006f56b108784cf2499bcabfd94cc4
Thomas White <thomas.white.ucf@gmail.com>
2019-11-03 11:01:05 -0500

    Sync

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const bodyParser = require("body-parser")
 app.use(express.static('public'));
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
-app.use(bodyParser.urlencoded({ extended : false }  ))
+app.use(bodyParser.urlencoded({ extended : true }  ))
 app.use(bodyParser.json());
 app.use(logger("dev"));
 
