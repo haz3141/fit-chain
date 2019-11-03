e61c87f58436e1874cacc6ca30d8fb85e60d87ad
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-11-03 01:11:11 -0400

    Cleaned up server.js

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
+// Import Dependencies ?? Refactor
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 3001;
 const logger = require('morgan');
-const app = express();
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser")
 
+// Set Port & Instantiate Express
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Import User Model ?? Remove
+const { User } = require('./models');
+
+// Configure Express Server ?? Refactor
 app.use(express.static('public'));
-app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : true }  ))
+app.use(logger("dev"));
 
-
-// Set Mongoose DB URI
+// Set MongoDB Path
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://haz3141:Pw3141!@ds241278.mlab.com:41278/heroku_pjvjkvk7";
 
-// Connect to the Mongo DB
+// Connect MongoDB
 mongoose
   .connect(MONGODB_URI, {
     useNewUrlParser: true,
@@ -25,13 +28,12 @@ mongoose
   })
   .then(console.log("DB CONNECTED: ", MONGODB_URI));
 
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// API Routes
+// GET Routes ?? Test
 app.get('/api/home', function(req, res) {
   res.send('Welcome!');
 });
@@ -39,19 +41,11 @@ app.get('/api/secret', function(req, res) {
   res.send('The password is potato');
 });
 
-// Import our User schema
-const { User } = require('./models');
-
-// console.log({User})
-
-// POST route to register a user
+// POST Route ?? Test
 app.post('/api/register', function(req, res) {
-  
-  console.log("req = ", req)
   const { email, password } = req.body;
-
   const user = new User({ email, password });
-  
+
   user.save(function(err) {
     if (err) {
       res.status(500)
@@ -62,13 +56,13 @@ app.post('/api/register', function(req, res) {
   });
 });
 
-
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+// Start Express Listener
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
