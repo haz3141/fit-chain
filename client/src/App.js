31d31284826db19b113db873f117e23d07584ac9
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-11-04 19:00:31 -0500

    Restructured App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,23 @@
-// Import Dependencies
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
 
-// Import Components
-import { Home } from "./components/Home";
-import Secret from "./components/Secret";
-import { Login } from "./components/Login";
-import { withAuth } from "./components/withAuth";
 
-class App extends Component {
-  render() {
+export default class App extends Component {  
+  state = {    
+    exercises: [],    
+    title: ''  
+  }
+  render() {    
     return (
-      <Router>
-        <div>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/secret">Secret</Link></li>
-            <li><Link to="/login">Login</Link></li>
-          </ul>
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/secret" component={withAuth(Secret)} />
-            <Route path="/login" component={Login} />
-          </Switch>
-        </div>
-      </Router>
+      <div>
+        <Typography variant='h1' align='center' gutterBottom>        
+        Exercises      
+      </Typography>
+      </div>
     );
   }
-}
 
-export { App };
+  handleChange = ({ target: { name, value } }) =>    this.setState({      [name]: value    })
+
+}
