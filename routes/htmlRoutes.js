196eab5a4d9194613462373afbedcc153aae4139
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-11-12 18:48:36 -0500

    Changed error in route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,6 +4,6 @@ module.exports = function(app) {
 	// Send every request to the React app
 	// Define any API routes before this runs
 	app.get('*', function(req, res) {
-		res.sendFile(path.join(__dirname, './client/build/index.html'));
+		res.sendFile(path.join(__dirname, '../client/build/index.html'));
 	});
 };
