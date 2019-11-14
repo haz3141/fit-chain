2251d7a35b674c3117e20489b84a821e5e83c007
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-11-13 23:46:16 -0500

    Push blockchain branch

--- client/src/components/DialogBox.js
+++ client/src/components/DialogBox.js
@@ -14,6 +14,8 @@ import PropTypes from 'prop-types';
 import AddIcon from '@material-ui/icons/Add';
 import { withStyles } from '@material-ui/styles';
 
+import axios from 'axios';
+
 // Styles to be passed in as Proptypes when rendering the component
 const styles = (theme) => ({
 	formControl: {
@@ -51,7 +53,18 @@ class DialogBox extends Component {
 	};
 
 	onSubmit = (event) => {
-		event.preventDefault();
+    event.preventDefault();
+    const payload = {
+      "data": {
+        "action": this.state.activity.action,
+        "description": this.state.activity.description,
+        "count": this.state.activity.count
+      }
+    };
+
+    axios.post('http://localhost:8080/mine', payload);
+    // let xhr = createCORSRequest('GET', 'http://localhost:8080');
+    // xhr.send();
 		fetch('/api/activity', {
 			method: 'POST',
 			body: JSON.stringify(this.state),
